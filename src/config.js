5d8ecb8400963b3a50a9c1ccd3892a7a254e9d56
SeungYunBaek <ryusim@cau.ac.kr>
2022-11-11 19:46:35 +0900

    update manifest and server config

--- src/config.js
+++
@@ -1,2 +0,0 @@
-export const API_SERVER_DOMAIN = "http://ec2-3-38-246-181.ap-northeast-2.compute.amazonaws.com"
-export const CLIENT_DOMAIN = window.location.protocol + "//" + window.location.hostname
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,7 +7,7 @@ const HomePage = () => {
       <p className={"main-description"}>티스토리에 소프트웨어 기술 블로그를 운영하고 계시다면, Quizlog에서 요약 퀴즈를 작성해 보세요.</p>
       <a className={"tistory-login"} href={"https://www.tistory.com/oauth/authorize" +
         "?client_id=2d3c1982f94273a5ddea6b2163071f4d" +
-        "&redirect_uri="+CLIENT_DOMAIN+":3000/auth" +
+        "&redirect_uri="+CLIENT_DOMAIN+"/auth" +
         "&response_type=code"}>Tistory로 로그인</a>
     </div>
   )
